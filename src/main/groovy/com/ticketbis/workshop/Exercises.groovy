15b523fe20c543c238f0c43b68cf119953f38438
Imanol Pinto Carral <ipintoc@gmail.com>
2016-02-25 10:38:19 +0100

    Basic tests completed

--- src/main/groovy/com/ticketbis/workshop/Exercises.groovy
+++ src/main/groovy/com/ticketbis/workshop/Exercises.groovy
@@ -1,7 +1,16 @@
 package com.ticketbis.workshop
 
 class Exercises {
-    double calculateTriangleArea(double base, double height) {
+
+    double calculateTriangleArea(Double base, Double height) {
+        if (hasInvalidTriangleArguments(base, height)) {
+            throw new IllegalArgumentException('Not valid arguments')
+        }
         (base * height) / 2
     }
+
+    private boolean hasInvalidTriangleArguments(Double base, Double height) {
+        !base || !height || base <= 0 || height <= 0
+    }
+
 }
--- src/test/groovy/com/ticketbis/workshop/Workshop01Spec.groovy
+++ src/test/groovy/com/ticketbis/workshop/Workshop01Spec.groovy
@@ -10,9 +10,14 @@ import spock.lang.Unroll
  * to handle exceptions and multiple data sets.
  */
 class Workshop01Spec extends Specification {
+
     def exercises = new Exercises()
 
-    def "calculate triangle area using the given base and height"() {
+    /**
+     * <p>TODO Write a feature method for {@link Exercises#calculateTriangleArea(Double, Double)}
+     * that calculates the triangle area using base = 3 and height = 2 values.</p>
+     */
+    def "Calculate triangle area using the given base and height"() {
         given: "the base and height"
         def base = 3
         def height = 2
@@ -24,6 +29,10 @@ class Workshop01Spec extends Specification {
         area == 3
     }
 
+    /**
+     * <p>TODO Write a feature method for {@link Exercises#calculateTriangleArea(Double, Double)}
+     * that calculates the triangle area using multiple base and height valid values.</p>
+     */
     @Unroll
     def "Calculate triangle area using multiple #base and #height data sets"() {
         expect: "triangle area calculate correctly"
@@ -34,5 +43,26 @@ class Workshop01Spec extends Specification {
         1    | 3      || 1.5
         2    | 2      || 2
         1.4  | 4      || 2.8
+    }
+
+    /**
+     * <p>TODO Write a feature method for {@link Exercises#calculateTriangleArea(Double, Double)}
+     * that checks the behaviour when either base and height could be negative, null or zero.
+     * An IllegalArgumentException should be thrown in this case.</p>
+     */
+    @Unroll
+    def "Handle invalid arguments when calculating triangle area (#base, #height)"() {
+        when: "calculating the triangle area with invalid arguments"
+        exercises.calculateTriangleArea(base, height)
+
+        then: "an illegalArgumentException is thrown"
+        def illegalArgumentException = thrown IllegalArgumentException
+        illegalArgumentException.message == "Not valid arguments"
+
+        where:
+        base | height
+        -1   | 3
+        2    | 0
+        null | 4
     }
 }
