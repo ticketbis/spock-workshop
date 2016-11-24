e26dc2712f3efeb13260e7ecb3a364c9aba64f24
Imanol Pinto <ipintoc@gmail.com>
2016-11-24 13:22:09 +0100

    Add workshop 2 solutions after rebase with master

--- src/test/groovy/com/ticketbis/workshop/Workshop02Spec.groovy
+++ src/test/groovy/com/ticketbis/workshop/Workshop02Spec.groovy
@@ -4,8 +4,11 @@ import com.ticketbis.workshop.library.Book
 import com.ticketbis.workshop.library.BookLoader
 import com.ticketbis.workshop.library.Library
 import groovy.json.JsonSlurper
+import spock.lang.Shared
 import spock.lang.Specification
 
+import static com.ticketbis.workshop.library.Book.Status.AVAILABLE
+import static com.ticketbis.workshop.library.Book.Status.UNAVAILABLE
 
 
 /**
@@ -18,25 +21,69 @@ import spock.lang.Specification
  * Also you will need to use the provide resource books.json file.
  */
 class Workshop02Spec extends Specification {
-    
+
+    Library library
+
+    @Shared
+    def books
+
+    def setupSpec() {
+        URL booksResource = this.getClass().getClassLoader().getResource("books.json")
+        books = getBookList(booksResource)
+    }
+
+    def setup() {
+        library = new Library()
+    }
+
     /**
      * <p>TODO Write a feature method for {@link Library#register(Book)}
      * in order to check that when we register a book it would be available to borrow.</p>
      */
+    def "When I register a book it should be available"() {
+        given: "a book"
+        Book book = new Book()
 
+        when: "adding a book to a library"
+        library.register(book)
+
+        then: "library has one book available"
+        library.books.first().status == AVAILABLE
+    }
 
     /**
      * <p>TODO Write a feature method for {@link Library#borrow(Book)}
      * in order to check that when we borrow a book it would be unavailable to borrow.</p>
      */
+    def "When I borrow a book it should be unavailable"() {
+        given: "a book registered in the library"
+        Book book = new Book(isbn: "123", status: AVAILABLE)
+        library.books << book
 
+        when: "I borrow a book"
+        Book borrowedBook = library.borrow(book)
+
+        then: "a book should be unavailable"
+        library.books.contains(borrowedBook)
+        borrowedBook.status == UNAVAILABLE
+    }
 
     /**
      * <p>TODO Write a feature method for {@link Library#borrow(Book)}
      * in order to check that when we try to borrow an unavailable book it would throw
      * an UnsupportedOperationException.</p>
      */
+    def "When I try to borrow an unavailable book it will throw an exception"() {
+        given: "a book registered in the library"
+        Book book = new Book(isbn: "123", status: UNAVAILABLE)
+        library.books << book
 
+        when: "I borrow a book"
+        library.borrow(book)
+
+        then: "a book should be unavailable"
+        thrown UnsupportedOperationException
+    }
 
     /**
      * <p>TODO Write a feature method for {@link BookLoader#register(Book)}
@@ -44,7 +91,16 @@ class Workshop02Spec extends Specification {
      *
      * <p>You will need to use the provide resource books.json file.</p>
      */
+    def "Should load a list of books using a file" () {
+        given: "a book loader"
+        BookLoader bookLoader = new BookLoader(library)
 
+        when: "a list of books is loaded"
+        bookLoader.register(books)
+
+        then: "the library should have all the books registered"
+        library.books.size() == old(library.books.size()) + 4
+    }
 
     /**
      * <p>TODO Write a feature method for {@link BookLoader#register(Book)}
@@ -53,10 +109,23 @@ class Workshop02Spec extends Specification {
      *
      * <p>You will need to use the provide resource books.json file.</p>
      */
+    def "Should load a list of books using a file adding only the new ones" () {
+        given: "a book loader"
+        BookLoader bookLoader = new BookLoader(library)
 
+        and: "a book that is in the file is previously registered"
+        def repeatedBook = new Book(title: "Other title", author: "Joshua Bloch", isbn: "0321356683")
+        library.register(repeatedBook)
 
-    private getBookList() {
-        URL booksResource = this.getClass().getClassLoader().getResource("books.json")
+        when: "a list of books is loaded"
+        bookLoader.register(books)
+
+        then: "the library should register only not repeated books"
+        library.books.size() == old(library.books.size()) + 3
+        repeatedBook in library.books
+    }
+
+    private getBookList(URL booksResource) {
         new JsonSlurper().parse(booksResource).books
     }
 
