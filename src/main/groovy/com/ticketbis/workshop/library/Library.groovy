e82cbe23a613e5bf14d1fb84632ec3bfe5e53d03
sdelamo <sergio.delamo@softamo.com>
2018-05-24 10:22:55 +0200

    Overload << operator for library

--- src/main/groovy/com/ticketbis/workshop/library/Library.groovy
+++ src/main/groovy/com/ticketbis/workshop/library/Library.groovy
@@ -14,6 +14,11 @@ class Library {
         books << book
     }
 
+    Library leftShift(final Book book) {
+        books << book
+        this
+    }
+
     Book borrow(Book book) {
         Book borrowedBook = books.find { it.isbn == book.isbn }
 
